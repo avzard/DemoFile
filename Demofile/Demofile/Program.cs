ad5e8f2ea18425a68f2b5a643587bba3f12161ac
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-06-30 12:43:27 +0530

    Branch  File

--- Demofile/Demofile/Program.cs
+++ Demofile/Demofile/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace Demofile
+
+namespace ObjectOrientedPrograms
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Object Oriented Program");
         }
     }
 }
